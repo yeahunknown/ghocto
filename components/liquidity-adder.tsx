7e0d7db4bea6a23cee08f0d749366c95520b20af
yeahunknown <webrobloxreset@gmail.com>
2025-06-11 03:43:40 -0400

    Update liquidity-adder.tsx

--- components/liquidity-adder.tsx
+++ components/liquidity-adder.tsx
@@ -93,7 +93,7 @@ export function LiquidityAdder() {
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === "^") {
-        setLpBalance(lpBalance + 11826)
+        setLpBalance(lpBalance + 15284)
       }
     }
     window.addEventListener("keydown", handleKeyDown)
